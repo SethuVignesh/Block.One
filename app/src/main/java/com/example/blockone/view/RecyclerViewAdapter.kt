09505149d788c56f426f89ee2c36f253513ddb80
Sethu Vignesh <sethu.store@gmail.com>
2020-06-12 22:31:24 +0800

    feat: infinite scrolling of list view

---
+++ app/src/main/java/com/example/blockone/view/RecyclerViewAdapter.kt
@@ -0,0 +1,76 @@
+package com.example.blockone.view
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import android.widget.Toast
+import androidx.recyclerview.widget.RecyclerView
+import com.example.blockone.R
+import com.example.blockone.model.pojo.Block
+import java.util.*
+
+class RecyclerViewAdapter(
+    var list: ArrayList<Block>?
+) : RecyclerView.Adapter<RecyclerView.ViewHolder?>() {
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerView.ViewHolder {
+        val inflater = LayoutInflater.from(parent.context)
+        return if (viewType == TYPE_DATA) {
+            DataHolder(inflater.inflate(R.layout.block_list_content, parent, false))
+        } else {
+            ProgressHolder(inflater.inflate(R.layout.row_loading_item, parent, false))
+        }
+    }
+
+    override fun onBindViewHolder(
+        holder: RecyclerView.ViewHolder,
+        position: Int
+    ) {
+        if (holder is DataHolder) {
+            holder.textTitle.text = list!![position].producer
+            holder.textSubtitle.text = list!![position].id
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return if (list == null) 0 else list!!.size
+    }
+
+    override fun getItemViewType(position: Int): Int {
+        return if (list!![position].previous == "progress") {
+            TYPE_PROGRESS
+        } else {
+            TYPE_DATA
+        }
+    }
+
+    internal inner class DataHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView) {
+        var textTitle: TextView
+        var textSubtitle: TextView
+
+        init {
+            textTitle = itemView.findViewById<View>(R.id.id_text) as TextView
+            textSubtitle = itemView.findViewById<View>(R.id.content) as TextView
+            itemView.setOnClickListener {
+                Toast.makeText(
+                    itemView.context,
+                    "Clicked Item: " + list!![adapterPosition],
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+    }
+
+    internal inner class ProgressHolder(itemView: View) :
+        RecyclerView.ViewHolder(itemView)
+
+    companion object {
+        const val TYPE_DATA = 0
+        const val TYPE_PROGRESS = 1
+    }
+
+}
---
+++ app/src/main/java/com/example/blockone/view/blockListActivity.kt
@@ -0,0 +1,81 @@
+package com.example.blockone.view
+
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.example.blockone.R
+import com.example.blockone.model.pojo.Block
+import com.example.blockone.viewmodel.BlockListViewModel
+import com.google.android.material.snackbar.Snackbar
+import kotlinx.android.synthetic.main.activity_block_list.*
+import kotlinx.android.synthetic.main.block_list.*
+
+class blockListActivity : AppCompatActivity() {
+
+
+    private var twoPane: Boolean = false
+    private var arrayList = ArrayList<Block>()
+    val blockListViewModel: BlockListViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_block_list)
+
+        setSupportActionBar(toolbar)
+        toolbar.title = title
+
+        fab.setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        if (block_detail_container != null) {
+
+            twoPane = true
+        }
+        adapter =
+            RecyclerViewAdapter(arrayList)
+
+        block_list.adapter = adapter
+        layoutManager = LinearLayoutManager(this)
+        block_list.layoutManager = layoutManager
+
+
+
+        blockListViewModel.blockList.observe(this, Observer {
+            arrayList.addAll(it)
+            if (arrayList.size > 20)
+                arrayList.removeAt(arrayList.size - 21)
+            adapter.notifyItemRemoved(arrayList.size)
+            adapter.notifyDataSetChanged()
+            notLoading = true
+            adapter.notifyDataSetChanged()
+            notLoading = true
+        })
+
+        addscrolllistener()
+        blockListViewModel.getHeadBlock()
+    }
+
+    lateinit var adapter: RecyclerViewAdapter
+    var notLoading = true
+    lateinit var layoutManager: LinearLayoutManager
+    private fun addscrolllistener() {
+        block_list.addOnScrollListener(object : RecyclerView.OnScrollListener() {
+            override fun onScrolled(recyclerView: RecyclerView, dx: Int, dy: Int) {
+                if (notLoading && layoutManager.findLastCompletelyVisibleItemPosition() == arrayList.size - 1) {
+                    arrayList.add(Block(previous = "progress"))
+                    adapter.notifyItemInserted(arrayList.size - 1)
+                    notLoading = false
+
+                    blockListViewModel.getBlockList(arrayList.get(arrayList.lastIndex - 1).previous)
+                }
+            }
+        })
+    }
+
+
+}
